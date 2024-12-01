0ce5ce0b6878fcf00912d5f6255c7e1744b6bf40
Aleksandar <acile067@gmail.com>
2024-12-01 09:03:39 +0100

    small update

--- Project_IKP_Load_Balancer/Worker/Thread_utils.cpp
+++ Project_IKP_Load_Balancer/Worker/Thread_utils.cpp
@@ -104,58 +104,7 @@ DWORD WINAPI ConnectToWorkersAndSendMsg(LPVOID lpParam)
 
             for (int i = 0; i < dequeuedElement->numPorts; i++)
             {
-                uint16_t targetPort = dequeuedElement->ports[i];
-
-                if (targetPort == yourPort) {
-                    printf("Skipping your own port: %u\n", yourPort);
-                    continue;
-                }
-
-                // Kreiranje socket-a za konekciju
-                SOCKET workerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-                if (workerSocket == INVALID_SOCKET) {
-                    printf("Failed to create socket for port: %u\n", targetPort);
-                    continue;
-                }
-
-                struct sockaddr_in workerAddr;
-                workerAddr.sin_family = AF_INET;
-                workerAddr.sin_port = htons(targetPort);
-                workerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-                // Povezivanje sa worker-om
-                if (connect(workerSocket, (struct sockaddr*)&workerAddr, sizeof(workerAddr)) == SOCKET_ERROR) {
-                    printf("Failed to connect to worker at port: %u\n", targetPort);
-                    closesocket(workerSocket);
-                    continue;
-                }
-
-                printf("Connected to worker at port: %u\n", targetPort);
-
-                // Slanje poruke
-                ClientMessage message;
-                message.clientName = dequeuedElement->clientName;
-                message.data = dequeuedElement->data;
-
-                char* serializedMessage;
-                int messageSize;
-
-                if (serialize_message(&message, &serializedMessage, &messageSize) == 0) {
-                    if (send(workerSocket, serializedMessage, messageSize, 0) == SOCKET_ERROR) {
-                        printf("Failed to send message to worker\n");
-                    }
-                    else {
-                        printf("Message sent to worker\n");
-                    }
-
-                    free(serializedMessage); // Oslobađanje memorije
-                }
-                else {
-                    printf("Failed to serialize message\n");
-                }
-
-                // Zatvaranje socket-a nakon poruke
-                closesocket(workerSocket);
+                ProcessPort(dequeuedElement->ports[i], yourPort, dequeuedElement);
             }
 
             // Oslobađanje memorije zauzete redom
---
+++ Project_IKP_Load_Balancer/run.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal
+
+rem Definišemo putanje do izvršnih fajlova (ažurirano za x64\Release)
+set LB_EXEC=x64\Debug\Load_Balancer.exe
+set CLIENT_EXEC=x64\Debug\Client.exe
+set WORKER_EXEC=x64\Debug\Worker.exe
+
+rem Proveravamo da li fajlovi postoje
+if not exist "%LB_EXEC%" (
+    echo Load Balancer nije pronađen na "%LB_EXEC%"
+    pause
+    exit /b
+)
+if not exist "%CLIENT_EXEC%" (
+    echo Client nije pronađen na "%CLIENT_EXEC%"
+    pause
+    exit /b
+)
+if not exist "%WORKER_EXEC%" (
+    echo Worker nije pronađen na "%WORKER_EXEC%"
+    pause
+    exit /b
+)
+
+rem Pokrećemo Load Balancer
+start cmd /k "%LB_EXEC%"
+
+rem Pokrećemo 2 klijenta
+start cmd /k "%CLIENT_EXEC%"
+start cmd /k "%CLIENT_EXEC%"
+
+rem Pokrećemo 3 workera
+start cmd /k "%WORKER_EXEC%"
+start cmd /k "%WORKER_EXEC%"
+start cmd /k "%WORKER_EXEC%"
+
+echo Svi procesi su pokrenuti.
+pause
